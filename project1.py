b89f6c744324d6ff9b4035f5135ba8d9c4a2cb51
Jai1747 <101503264+Jai1747@users.noreply.github.com>
2024-02-13 16:18:51 +0100

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,21 @@
+#%%
+from ucimlrepo import fetch_ucirepo 
+import importlib_resources
+import numpy as np
+import xlrd
+  
+# fetch dataset 
+abalone = fetch_ucirepo(id=1) 
+  
+# data (as pandas dataframes) 
+X = abalone.data.features 
+y = abalone.data.targets 
+  
+# metadata 
+print(abalone.metadata) 
+  
+# variable information 
+print(abalone.variables) 
+
+
+# %%
